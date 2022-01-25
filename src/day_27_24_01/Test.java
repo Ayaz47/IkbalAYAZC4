ac88b6065843aa920739fb28a6c620c73fa0189b
Ayaz47 <m.ikbalayaz14@gmail.com>
2022-01-25 04:00:27 -0500

    d

---
+++ src/day_27_24_01/Test.java
@@ -0,0 +1,16 @@
+package day_27_24_01;
+
+public class Test {
+    public static void main(String[] args) {
+        My m = new Foo()  ;
+        Tempo t = (Tempo)m;//downcasting
+        Foo f  = (Foo) t  ;//downcasting
+    }
+}
+
+
+interface My{}
+
+abstract class Tempo implements My{}
+
+class Foo extends Tempo{}
